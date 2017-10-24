5f82abad7a6f9386776c96056e8b123fc67a74a0
Zodraccir <zichittella.riccardo@gmail.com>
2017-10-24 17:19:31 +0200

    aggiunto

--- main3.py
+++ main3.py
@@ -24,7 +24,8 @@ def printer_errorneous_state(printer, error_states=PRINTER_ERROR_STATES):
 def printCatching(printJob):
     for job in printJob:
         for key,values in job.items():
-            print(str(key)+"  ==> value ==> "+str(values))
+            if key=="Status" && values=="None":
+                print(str(key)+"  ==> value ==> "+str(values))
             pass
         return job
 
